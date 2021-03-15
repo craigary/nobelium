60704d17e3edf174b250cf7281564ce80faacdbe
Craig Hart <i@craigary.net>
2021-03-16 01:55:14 +0800

    :clown_face: clean the code and tweak the style

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -5,13 +5,13 @@ import formatDate from '@/lib/formatDate'
 const BlogPost = ({ post }) => {
   return (
     <article key={post.id} className="mb-6 md:mb-8">
-      <header className="flex flex-col justify-between md:flex-row md:items-center">
+      <header className="flex flex-col justify-between md:flex-row md:items-start">
         <Link href={`${BLOG.path}/${post.slug}`}>
           <h2 className="font-sans text-lg md:text-xl font-medium mb-2 cursor-pointer">
             <a>{post.title}</a>
           </h2>
         </Link>
-        <time className="flex-shrink-0 text-gray-600  md:mb-2">{formatDate(post.date, BLOG.lang)}</time>
+        <time className="flex-shrink-0 text-gray-600">{formatDate(post.date, BLOG.lang)}</time>
       </header>
       <main>
         <p className="hidden md:block text-gray-700 leading-8">{post.summary}</p>
