06fee1bf787896a720f5296043e7b8d3b62ded98
Craig Hart <i@craigary.net>
2021-03-20 01:08:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,7 @@ const BLOG = {
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'zh-CN',
-  appearance: ['light', 'dark', 'auto']
+  appearance: ['light', 'dark', 'auto'],
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // if leave this empty, current year will be used.
   postsPerPage: 7,
