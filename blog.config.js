1194d6a7d60b6010e3b5f73cc0dcd2989fc9956f
ice-Yaya <73091684+ice-Yaya@users.noreply.github.com>
2021-03-19 23:05:07 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,8 +3,8 @@ const BLOG = {
   author: 'Margaret',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
-  description: '',
-  lang: 'zh-CN',
+  description: 'I have no idea how I should decribe this.',
+  lang: 'en-US',
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // if leave this empty, current year will be used.
   postsPerPage: 4,
