9f4902a142b0aa7eb3b716ee9f4bc1f445193399
Craig Hart <i@craigary.net>
2021-03-21 14:18:24 +0800

    :truck: Add footer link

--- components/Footer.js
+++ components/Footer.js
@@ -4,12 +4,12 @@ const Footer = () => {
   const y = d.getFullYear()
   return (
     <div className="mt-6 flex-shrink-0 m-auto max-w-3xl px-4 font-noEmoji w-full text-gray-600 dark:text-gray-400">
-      <hr />
+      <hr className="dark:border-gray-600" />
       <div className="my-4 text-sm leading-6 font-medium">
-        <p>Built with ♥ and ⚛ Next.js. Proudly deployed on ▲Vercel.</p>
+        <p><a href="https://github.com/craigary/nobelium" target="_blank" rel="noreferrer">Nobelium</a> is built with ♥ and ⚛ Next.js. Proudly deployed on ▲Vercel.</p>
         <p>
-          ©{BLOG.author}{' '}
-          {BLOG.since === y || !BLOG.since ? y : `${BLOG.since} - ${y}`}{' '}
+          © {BLOG.author}{' '}
+          {BLOG.since === y || !BLOG.since ? y : `${BLOG.since} - ${y}`}
         </p>
       </div>
     </div>
