a781fc742c28bacf3d324e371d172e5e90da09af
Craig Hart <i@craigary.net>
2021-03-21 14:44:34 +0800

    :lipstick: Tweak nav bar and footer a bit

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   const y = d.getFullYear()
   return (
     <div className="mt-6 flex-shrink-0 m-auto max-w-3xl px-4 font-noEmoji w-full text-gray-600 dark:text-gray-400">
-      <hr className="dark:border-gray-600" />
+      <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6 font-medium">
         <p><a href="https://github.com/craigary/nobelium" target="_blank" rel="noreferrer">Nobelium</a> is built with ♥ and ⚛ Next.js. Proudly deployed on ▲Vercel.</p>
         <p>
