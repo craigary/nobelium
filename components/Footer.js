e4fa22db81ae177c251e9b777f20b293e7e1e0e7
Craig Hart <i@craigary.net>
2021-03-24 20:15:00 +0800

    :lipstick: Change full page padding

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ const Footer = ({ fullWidth }) => {
   const d = new Date()
   const y = d.getFullYear()
   return (
-    <div className={`mt-6 flex-shrink-0 m-auto px-4 font-noEmoji w-full text-gray-600 dark:text-gray-400${!fullWidth ? 'max-w-3xl' : ''}`}>
+    <div className={`mt-6 flex-shrink-0 m-auto font-noEmoji w-full text-gray-600 dark:text-gray-400 ${!fullWidth ? 'max-w-3xl px-4' : 'px-24'}`}>
       <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6 font-medium">
         <p><a href="https://github.com/craigary/nobelium" target="_blank" rel="noreferrer">Nobelium</a> is built with ♥ and ⚛ Next.js. Proudly deployed on ▲Vercel.</p>
