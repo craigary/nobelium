d6b39d282972a9f5caa8970753538fff060f3d56
Craig Hart <i@craigary.net>
2021-04-01 23:52:04 +0800

    :mag: SEO

--- components/Container.js
+++ components/Container.js
@@ -3,6 +3,7 @@ import Footer from '@/components/Footer'
 import BLOG from '@/blog.config'
 import Head from 'next/head'
 import PropTypes from 'prop-types'
+import BlogPost from './BlogPost'
 
 const Container = ({ children, layout, fullWidth, ...customMeta }) => {
   const meta = {
@@ -13,7 +14,6 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
   return (
     <div>
       <Head>
-        {/* Add more Meta Info */}
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         {BLOG.seo.googleSiteVerification && (
@@ -26,12 +26,36 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
           <meta name="keywords" content={BLOG.seo.keywords.join(', ')} />
         )}
         <meta name="description" content={meta.description} />
+        <meta property="og:locale" content={BLOG.lang} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:type" content={layout || 'Blog'} />
+        <meta property="og:description" content={meta.description} />
+        <meta
+          property="og:url"
+          content={
+            BLOG.path
+              ? `${BLOG.link}/${meta.slug}/${meta.slug}`
+              : `${BLOG.link}/${meta.slug}`
+          }
+        />
         <meta
           property="og:image"
           content={`https://og-image-craigary.vercel.app/${meta.title}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg`}
         />
+
+        <meta name="twitter:card" content={meta.description} />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta
+          name="twitter:image"
+          content={`https://og-image-craigary.vercel.app/${meta.title}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg`}
+        />
+        {meta.type === 'article' && (
+          <>
+            <meta property="article:published_time" content={meta.date} />
+            <meta property="article:author" content={BLOG.author} />
+            <meta property="article:tag" content={meta.tags.join(', ') || ''} />
+          </>
+        )}
       </Head>
       <div className="wrapper">
         <Header
