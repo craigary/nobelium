2e7a5766ca71136eae2a21d3712f87c705fdc296
ice-Yaya <73091684+ice-Yaya@users.noreply.github.com>
2021-04-02 16:45:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,16 +1,19 @@
 const BLOG = {
-  title: 'M',
-  author: 'Margaret',
+  title: 'CRAIGARY',
+  author: 'Craig Hart',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
-  description: 'I have no idea how I should decribe this.✨',
+  description: 'This gonna be an awesome website.',
   lang: 'en-US',
+  appearance: 'auto', // ['light', 'dark', 'auto'],
+  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: '#111827', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // if leave this empty, current year will be used.
-  postsPerPage: 4,
+  postsPerPage: 7,
   showAbout: true, // WIP
   showArchive: true, // WIP
-  socialLink: 'https://t.me/iceyaya',
+  socialLink: 'https://twitter.com/craigaryhart',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
@@ -19,15 +22,16 @@ const BLOG = {
   analytics: {
     provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
-      tracker: 'https://ackee.craigary.net/tracker.js', // e.g 'https://ackee.craigary.net/tracker.js'
-      dataAckeeServer: 'https://ackee.craigary.net/', // e.g https://ackee.craigary.net , don't end with a slash
-      domainId: '0e2257a8-54d4-4847-91a1-0311ea48cc7b' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+      tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
+      dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
+      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'G-PPKMNRCKJK' // e.g: G-XXXXXXXXXX
+      measurementId: '' // e.g: G-XXXXXXXXXX
     }
   },
-  comment: { // support provider: gitalk
+  comment: {
+    // support provider: gitalk
     provider: '', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
@@ -39,5 +43,5 @@ const BLOG = {
     }
   }
 }
-
-export default BLOG
+// export default BLOG
+module.exports = BLOG
