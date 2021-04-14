98976055c290792162e3375c23bdb66692fee532
Craig Hart <i@craigary.net>
2021-04-14 22:15:00 +0800

    :bug: Fix Fonts, again!

--- pages/_document.js
+++ pages/_document.js
@@ -56,26 +56,26 @@ class MyDocument extends Document {
             <>
               <link
                 rel="preconnect"
-                href="https://gstatic.loli.net"
+                href="https://fonts.gstatic.com"
                 crossOrigin="anonymous"
               />
               <link
                 rel="preload"
                 as="style"
-                href={`https://fonts.loli.net/css2?family=Noto+${
+                href={`https://fonts.googleapis.com/css2?family=Noto+${
                   BLOG.font === 'serif' ? 'Serif' : 'Sans'
                 }+${CJK()}:wght@400;500;700&display=swap`}
               />
               <link
                 rel="stylesheet"
-                href={`https://fonts.loli.net/css2?family=Noto+${
+                href={`https://fonts.googleapis.com/css2?family=Noto+${
                   BLOG.font === 'serif' ? 'Serif' : 'Sans'
                 }+${CJK()}:wght@400;500;700&display=swap`}
               />
               <noscript>
                 <link
                   rel="stylesheet"
-                  href={`https://fonts.loli.net/css2?family=Noto+${
+                  href={`https://fonts.googleapis.com/css2?family=Noto+${
                     BLOG.font === 'serif' ? 'Serif' : 'Sans'
                   }+${CJK()}:wght@400;500;700&display=swap`}
                 />
