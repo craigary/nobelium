8d06a3e72b1b039508b632f2df8dcd520b3e4236
Reynard <oyrx@vip.qq.com>
2021-05-04 18:54:20 +0800

    [Feature] Add CollapsedNavBar config
    The automatically collapsed navigation bar

--- blog.config.js
+++ blog.config.js
@@ -15,6 +15,7 @@ const BLOG = {
   sortByDate: false,
   showAbout: true, // WIP
   showArchive: true, // WIP
+  autoCollapsedNavBar: false, // the automatically collapsed navigation bar
   socialLink: 'https://twitter.com/craigaryhart',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
