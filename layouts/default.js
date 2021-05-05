a39f649cc4e26ea82b8ad8281257ee4df0e2f07f
yokinist <ikd618@gmail.com>
2021-05-06 01:25:26 +0900

    :fire: delete unused key  of tag text

--- layouts/default.js
+++ layouts/default.js
@@ -75,7 +75,6 @@ const DefaultLayout = ({ children, blockMap, frontMatter }) => {
                   <Link key={tag} href={`/tag/${encodeURIComponent(tag)}`}>
                     <a>
                       <p
-                        key={tag}
                         className="mr-1"
                       >
                         #{tag}
