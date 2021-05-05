b444510dd370ae4fbf724f04d48b064b721c01eb
yokinist <ikd618@gmail.com>
2021-05-06 00:47:20 +0900

    :recycle: refactor tag class

--- components/Tags.js
+++ components/Tags.js
@@ -11,10 +11,10 @@ const Tags = ({ tags, currentTag }) => {
             <Link key={key} href={selected ? '/search' : `/tag/${encodeURIComponent(key)}`}>
               <a>
                 <li
-                  className={`mr-3 py-2 font-medium border px-4 whitespace-nowrap  ${
+                  className={`mr-3 py-2 font-medium border px-4 whitespace-nowrap dark:text-gray-300 ${
                     selected
-                      ? 'text-white bg-black border-black dark:bg-gray-600 dark:border-gray-600 dark:text-gray-300'
-                      : 'bg-gray-100 border-gray-100 text-gray-400 dark:bg-night dark:border-gray-800 dark:text-gray-300'
+                      ? 'text-white bg-black border-black dark:bg-gray-600 dark:border-gray-600'
+                      : 'bg-gray-100 border-gray-100 text-gray-400 dark:bg-night dark:border-gray-800'
                   }`}
                 >
                   {`${key} (${tags[key]})`}
