1836b53162be80112466ed7413d7c8ebf69e720d
yokinist <ikd618@gmail.com>
2021-05-05 23:50:25 +0900

    :wheelchair: fix tag link to use next/link in search page

--- components/Tags.js
+++ components/Tags.js
@@ -1,21 +1,27 @@
-const Tags = ({ tags, handleTagClick, selectedTag }) => {
+import Link from 'next/link'
+
+const Tags = ({ tags, currentTag }) => {
   if (!tags) return null
   return (
     <div className="tag-container">
       <ul className="flex max-w-full mt-4 overflow-x-auto">
-        {Object.keys(tags).map(key => (
-          <li
-            key={key}
-            onClick={() => handleTagClick(key)}
-            className={`mr-3 py-2 font-medium cursor-pointer  border px-4 whitespace-nowrap bg-gray-100 border-gray-100 text-gray-400 dark:bg-night dark:border-gray-800 dark:text-gray-300 ${
-              key === selectedTag
-                ? 'text-white bg-black border-black dark:bg-gray-600 dark:border-gray-600 dark:text-gray-300'
-                : ''
-            }`}
-          >
-            {`${key} (${tags[key]})`}
-          </li>
-        ))}
+        {Object.keys(tags).map(key => {
+          const selected = key === currentTag;
+          return (
+            <Link key={key} href={selected ? '/search' : `/tag/${encodeURIComponent(key)}`}>
+              <a>
+                <li
+                  className={`mr-3 py-2 font-medium border px-4 whitespace-nowrap bg-gray-100 border-gray-100 text-gray-400 dark:bg-night dark:border-gray-800 dark:text-gray-300 ${
+                    selected
+                      ? 'text-white bg-black border-black dark:bg-gray-600 dark:border-gray-600 dark:text-gray-300'
+                      : ''
+                  }`}
+                >
+                  {`${key} (${tags[key]})`}
+                </li>
+              </a>
+            </Link>
+        )})}
       </ul>
     </div>
   )
