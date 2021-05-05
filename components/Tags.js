2e53ac03aad304fdbc45e20eabf0fca2bc39e862
yokinist <ikd618@gmail.com>
2021-05-05 23:53:31 +0900

    :lipstick: fix selected tag style

--- components/Tags.js
+++ components/Tags.js
@@ -11,10 +11,10 @@ const Tags = ({ tags, currentTag }) => {
             <Link key={key} href={selected ? '/search' : `/tag/${encodeURIComponent(key)}`}>
               <a>
                 <li
-                  className={`mr-3 py-2 font-medium border px-4 whitespace-nowrap bg-gray-100 border-gray-100 text-gray-400 dark:bg-night dark:border-gray-800 dark:text-gray-300 ${
+                  className={`mr-3 py-2 font-medium border px-4 whitespace-nowrap  ${
                     selected
                       ? 'text-white bg-black border-black dark:bg-gray-600 dark:border-gray-600 dark:text-gray-300'
-                      : ''
+                      : 'bg-gray-100 border-gray-100 text-gray-400 dark:bg-night dark:border-gray-800 dark:text-gray-300'
                   }`}
                 >
                   {`${key} (${tags[key]})`}
