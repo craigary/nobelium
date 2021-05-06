421b51dde94e93044979dba9a302e682932cc3b3
LJAYi <37037437+LJAYi@users.noreply.github.com>
2021-05-06 23:15:25 +0800

    Revert ":bug: Fix sort by date not working & more"
    This reverts commit 5c6454bad486c878780d81a029638df22bf71e0c.

--- blog.config.js
+++ blog.config.js
@@ -12,7 +12,7 @@ const BLOG = {
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // if leave this empty, current year will be used.
   postsPerPage: 7,
-  sortByDate: true,
+  sortByDate: false,
   showAbout: true, // WIP
   showArchive: true, // WIP
   socialLink: 'https://twitter.com/craigaryhart',
