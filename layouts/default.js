259efb5dcbae1de60aebf71c97a1a292f62a1b73
boycottangent <1206469487@qq.com>
2021-05-13 10:24:27 +0800

    Fix avatar not showing bug

--- layouts/default.js
+++ layouts/default.js
@@ -56,7 +56,7 @@ const DefaultLayout = ({ children, blockMap, frontMatter, emailHash }) => {
                   alt={BLOG.author}
                   width={24}
                   height={24}
-                  src={`https://gravatar.com/avatar/${emailHash}`}
+                  src="/avatar.svg"
                   className="rounded-full"
                 />
                 <p className="ml-2 md:block">{BLOG.author}</p>
