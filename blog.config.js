deeb7f25df45599893a09554ce4ce2618f6ec150
quangIELogEsIc <84220976+quangIELogEsIc@users.noreply.github.com>
2021-05-15 16:29:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'test@qq.com',
   link: 'https://i.1990day.com',
   description: '记录生活与照片分享',
-  lang: 'en-US',
+  lang: 'zh-CN',
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
