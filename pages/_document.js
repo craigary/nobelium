c658ce77cc6aa1e533430440f3340ee5f809af40
Craig Hart <i@craigary.net>
2021-06-27 20:40:07 +0800

    :sparkles: Support Safari 15 new tab bar design

--- pages/_document.js
+++ pages/_document.js
@@ -86,6 +86,17 @@ class MyDocument extends Document {
           )}
           <link rel="icon" href="/favicon.ico" />
           <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
+          {BLOG.appearance === 'auto'
+            ? (
+            <>
+            <meta name="theme-color" content={BLOG.lightBackground} media="(prefers-color-scheme: light)"/>
+            <meta name="theme-color" content={BLOG.darkBackground} media="(prefers-color-scheme: dark)"/>
+            </>
+              )
+            : (
+            <meta name="theme-color" content={BLOG.appearance === 'dark' ? BLOG.darkBackground : BLOG.lightBackground} />
+              )
+          }
         </Head>
         <body className="bg-day dark:bg-night">
           <Main />
