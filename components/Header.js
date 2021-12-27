5bd599eace5ed24d40822b092b22625952d687be
Yoon Seongbin <holmes9595@mail.ulsan.ac.kr>
2021-12-27 18:20:27 +0900

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ const NavBar = () => {
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-//     { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
+    // { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
     { id: 2, name: 'Study Log', to: '/study', show: true },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
