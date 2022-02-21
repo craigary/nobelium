cd8275c9c315a7f2bdd0638ed73e4adf8d691a1f
Seongbin Yoon <holmes9595@mail.ulsan.ac.kr>
2022-02-21 10:39:58 +0900

    tidy links up

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,8 @@ const NavBar = () => {
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     // { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
-    { id: 2, name: 'Study Log', to: '/study', show: true },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
+    // { id: 2, name: 'Study Log', to: '/study', show: true },
+    { id: 2, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
