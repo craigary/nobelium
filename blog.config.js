7bc20d9164e348865ea377f1f729bf02e68daa62
he_tao <53343436+hetaozdh@users.noreply.github.com>
2022-05-22 10:35:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -32,7 +32,7 @@ const BLOG = {
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'G-FBY8QE2R8R' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-C78TP9KS53' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
