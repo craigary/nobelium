52bdacbc9f7b61ab5592f8a857574bdecb80cbe6
JimYan <sansanboy@users.noreply.github.com>
2022-05-27 00:37:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/MoMaDa2022',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['momada blog', '技术日志', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
