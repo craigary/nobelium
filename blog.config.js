1d163c1e2b82b5d8cbced3525efd71ddc1e69e42
JimYan <sansanboy@users.noreply.github.com>
2022-09-05 16:00:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,5 +1,5 @@
 const BLOG = {
-  title: 'MoMaDa Note',
+  title: 'DaJun Blog',
   author: 'JimYan',
   email: 'nefu.yzj@gmail.com',
   link: 'https://dajun.info',
@@ -11,7 +11,7 @@ const BLOG = {
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2022, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  postsPerPage: 5,
   sortByDate: true,
   showAbout: true,
   showArchive: true,
