872d0b926eac069c680f9f741e74b1a7c7420a8a
Leidi <leidison@163.com>
2022-11-03 13:38:02 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ const Footer = ({ fullWidth }) => {
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}❤️
           </p>
           <Vercel />
         </div>
