688ae5a69cd24b80d7236d52aca48dbc058141ba
mindflip <hmu1212@hotmail.com>
2022-11-19 21:37:49 +0900

    fix: config link

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@ const BLOG = {
   title: 'Mindflip',
   author: 'Park Neungeom',
   email: 'hmu1212@hotmail.com',
-  link: 'https://nobelium.vercel.app',
+  link: 'https://blog-mindflip.vercel.app/',
   description: 'Software Engineer',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
