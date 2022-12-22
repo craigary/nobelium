1a84069241f8680a3dfd630d81753720742f7f0d
JimYan <sansanboy@users.noreply.github.com>
2022-12-22 15:13:39 +0800

    Create blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -11,7 +11,7 @@ const BLOG = {
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2022, // If leave this empty, current year will be used.
-  postsPerPage: 5,
+  postsPerPage: 7,
   sortByDate: true,
   showAbout: true,
   showArchive: true,
