33a802ac2b69956e6bda42e45ade0f8d6ff3a18a
SilentDepth <github@silent.land>
2023-03-22 14:58:47 +0800

    Download config from Notion before build

---
+++ scripts/prebuild.mjs
@@ -0,0 +1,39 @@
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import fs from 'node:fs'
+import nextEnv from '@next/env'
+import { NotionAPI } from 'notion-client'
+import { getTextContent } from 'notion-utils'
+
+const ROOT = resolve(fileURLToPath(import.meta.url), '../..')
+
+// Only fetch config from Notion when no local config file is found
+if (!fs.existsSync(resolve(ROOT, 'blog.config.js'))) {
+  console.log('Local config not found. Fetch from Notion...')
+
+  nextEnv.loadEnvConfig(ROOT)
+  const {
+    NOTION_PAGE_ID,
+    NOTION_ACCESS_TOKEN,
+    VERCEL_ENV
+  } = process.env
+
+  const api = new NotionAPI()
+  const everything = await api.getPage(NOTION_PAGE_ID)
+  const [typePropId] = Object.entries(Object.values(everything.collection)[0].value.schema).find(([id, value]) => value.name === 'type')
+  const [, { value: configPage }] = Object.entries(everything.block).find(([id, { value }]) => getTextContent(value.properties?.[typePropId]) === 'Config')
+  const configCodeBlockId = configPage.content.find(id => everything.block[id].value.type === 'code')
+  const configCodeBlock = everything.block[configCodeBlockId].value
+  const config = JSON.parse(getTextContent(configCodeBlock.properties.title))
+
+  // Append private fields
+  Object.assign(config, {
+    notionPageId: NOTION_PAGE_ID,
+    notionAccessToken: NOTION_ACCESS_TOKEN,
+    isProd: VERCEL_ENV === 'production'
+  })
+
+  fs.writeFileSync(resolve(ROOT, 'blog.config.js'), `const config = ${JSON.stringify(config, null, 2)}\nexport default config`, 'utf-8')
+
+  console.log('Remote config fetched successfully')
+}
