ec659d70e49b5408ff39de379fdf430f706d81e9
BeautyyuYanli <beautyyuyanli@gmail.com>
2023-03-25 01:24:08 +0800

    feat: Use favorion on header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useRef } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import BLOG from '@/blog.config'
 import { useLocale } from '@/lib/locale'
 
@@ -66,7 +67,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
       <div
         className={`sticky-nav group m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-12 py-8 bg-opacity-60 ${
           !fullWidth ? 'max-w-3xl px-4' : 'px-4 md:px-24'
-        }`}
+          }`}
         id="sticky-nav"
         ref={navRef}
         onClick={handleClickHeader}
@@ -83,33 +84,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <div className="flex items-center">
           <Link href="/" aria-label={BLOG.title}>
             <div className="h-6">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  width="24"
-                  height="24"
-                  className="fill-current text-black dark:text-white"
-                />
-                <rect width="24" height="24" fill="url(#paint0_radial)" />
-                <defs>
-                  <radialGradient
-                    id="paint0_radial"
-                    cx="0"
-                    cy="0"
-                    r="1"
-                    gradientUnits="userSpaceOnUse"
-                    gradientTransform="rotate(45) scale(39.598)"
-                  >
-                    <stop stopColor="#CFCFCF" stopOpacity="0.6" />
-                    <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
-                  </radialGradient>
-                </defs>
-              </svg>
+              <Image src="/favicon.ico" alt="favicon" width="24" height="24"></Image>
             </div>
           </Link>
           {navBarTitle ? (
