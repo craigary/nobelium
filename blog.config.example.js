c3d126aef10f03bcea5460d0b1ab59bb1a079f91
SilentDepth <github@silent.land>
2023-03-29 11:59:17 +0800

    Add comments to example config

--- blog.config.example.js
+++ blog.config.example.js
@@ -1,70 +1,117 @@
 module.exports = {
   /* Site info */
 
+  // Site title
   title: 'CRAIGARY',
+  // Site description, will be displayed next to the site title
   description: 'This gonna be an awesome website.',
+  // Site URL
   link: 'https://nobelium.vercel.app',
-  since: 2021, // If leave this empty, current year will be used.
+  // Site creation year, will use the current year if unset
+  since: 2021,
+  // Author name
   author: 'Craig Hart',
+  // Author email, will determine the avatar returned by Gravatar
   email: 'i@craigary.net',
+  // Author’s social account link, will become the link address of the author’s avatar
   socialLink: 'https://twitter.com/craigaryhart',
 
   /* Site preferences */
 
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'auto', // ['light', 'dark', 'auto'],
-  font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#18181B', // use hex value, don't forget '#'
+  // Site language, can be one of these:
+  //   'en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES'
+  lang: 'en-US',
+  // Site time zone, will determine the time zone of article time (using IANA identifier format)
+  // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options
+  timezone: 'Asia/Shanghai',
+  // Site appearance, can be one of these:
+  //   'light', 'dark', 'auto'
+  appearance: 'auto',
+  // Site font, can be one of these:
+  //   'sans-serif', 'serif'
+  font: 'sans-serif',
+  // Light mode background color in #RRGGBB form
+  lightBackground: '#ffffff',
+  // Dark mode background color in #RRGGBB form
+  darkBackground: '#18181B',
+  // Number of articles per page in article list
   postsPerPage: 7,
+  // Whether to sort posts by date
   sortByDate: false,
+  // Whether to display “About” link
   showAbout: true,
-  autoCollapsedNavBar: false, // The automatically collapsed navigation bar
+  // Always use a collapsed navigation bar
+  autoCollapsedNavBar: false,
 
   /* Technical config */
 
-  path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  // Site root path. If you want to mount the site under a certain path of another site, you need to fill in this item.
+  path: '',
+  // OG image generation service address (don’t add a slash at the end)
+  ogImageGenerateURL: 'https://og-image-craigary.vercel.app',
+  // SEO configuration
   seo: {
+    // Site keywords
     keywords: ['Blog', 'Website', 'Notion'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+    // Google site verification code
+    googleSiteVerification: ''
   },
 
   /* Analytics config */
 
+  // Traffic statistics tool configuration
   analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    // Traffic statistics tool, can be one of these: (leave it empty to disable)
+    //   'ga', 'ackee'
+    provider: '',
+    // Ackee configuration
     ackeeConfig: {
-      tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
-      dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
-      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+      // Tracker address (e.g. 'https://ackee.craigary.net/tracker.js')
+      tracker: '',
+      // Tracking server address (e.g. 'https://ackee.craigary.net', don’t add a slash at the end)
+      dataAckeeServer: '',
+      // e.g. '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+      domainId: ''
     },
+    // Google Analytics configuration
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      // G-Tag (e.g. G-XXXXXXXXXX)
+      measurementId: ''
     }
   },
 
   /* Comment config */
 
+  // Comment tool configuration
   comment: {
-    // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    // Comment tool, can be one of these: (leave it empty to disable)
+    //   'gitalk', 'utterances', 'cusdis'
+    provider: '',
+    // Gitalk configuration
     gitalkConfig: {
-      repo: '', // The repository of store comments
+      // Repository name
+      repo: '',
+      // Repository owner
       owner: '',
+      // Administrator
       admin: [],
       clientID: '',
       clientSecret: '',
+      // Whether to enable distraction-free mode
       distractionFreeMode: false
     },
+    // Utterances configuration
     utterancesConfig: {
+      // Repository name
       repo: ''
     },
+    // Cusdis configuration
     cusdisConfig: {
-      appId: '', // data-app-id
-      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
-      scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
+      appId: '',
+      // Service address, only need to modify when using self-hosted service
+      host: 'https://cusdis.com',
+      // Script address, only need to modify when using self-hosted service
+      scriptSrc: 'https://cusdis.com/js/cusdis.es.js'
     }
   }
 }
