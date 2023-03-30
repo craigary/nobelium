0503ca5e9bb72c7d944f20dec285af5a16a27041
SilentDepth <github@silent.land>
2023-03-30 21:53:08 +0800

    feat: Mermaid support
    Close #213

--- components/NotionRenderer.js
+++ components/NotionRenderer.js
@@ -1,49 +1,68 @@
+import { createElement as h } from 'react'
 import dynamic from 'next/dynamic'
 import { NotionRenderer as Renderer } from 'react-notion-x'
+import { getTextContent } from 'notion-utils'
 import BLOG from '@/blog.config'
 import tailwindConfig from '@/tailwind.config'
 
 // Lazy-load some heavy components
 const components = {
   // Code block
-  Code: dynamic(() => {
-    return import('react-notion-x/build/third-party/code').then(async module => {
-      // Additional prismjs syntax
-      await Promise.all([
-        import('prismjs/components/prism-markup-templating'),
-        import('prismjs/components/prism-markup'),
-        import('prismjs/components/prism-bash'),
-        import('prismjs/components/prism-c'),
-        import('prismjs/components/prism-cpp'),
-        import('prismjs/components/prism-csharp'),
-        import('prismjs/components/prism-docker'),
-        import('prismjs/components/prism-java'),
-        import('prismjs/components/prism-js-templates'),
-        import('prismjs/components/prism-coffeescript'),
-        import('prismjs/components/prism-diff'),
-        import('prismjs/components/prism-git'),
-        import('prismjs/components/prism-go'),
-        import('prismjs/components/prism-graphql'),
-        import('prismjs/components/prism-handlebars'),
-        import('prismjs/components/prism-less'),
-        import('prismjs/components/prism-makefile'),
-        import('prismjs/components/prism-markdown'),
-        import('prismjs/components/prism-objectivec'),
-        import('prismjs/components/prism-ocaml'),
-        import('prismjs/components/prism-python'),
-        import('prismjs/components/prism-reason'),
-        import('prismjs/components/prism-rust'),
-        import('prismjs/components/prism-sass'),
-        import('prismjs/components/prism-scss'),
-        import('prismjs/components/prism-solidity'),
-        import('prismjs/components/prism-sql'),
-        import('prismjs/components/prism-stylus'),
-        import('prismjs/components/prism-swift'),
-        import('prismjs/components/prism-wasm'),
-        import('prismjs/components/prism-yaml')
-      ])
-      return module.Code
-    })
+  Code: dynamic(async () => {
+    return function CodeSwitch (props) {
+      switch (getTextContent(props.block.properties.language)) {
+        case 'Mermaid':
+          return h(
+            dynamic(() => {
+              return import('@/components/notion-blocks/Mermaid').then(module => module.default)
+            }, { ssr: false }),
+            props
+          )
+        default:
+          return h(
+            dynamic(() => {
+              return import('react-notion-x/build/third-party/code').then(async module => {
+                // Additional prismjs syntax
+                await Promise.all([
+                  import('prismjs/components/prism-markup-templating'),
+                  import('prismjs/components/prism-markup'),
+                  import('prismjs/components/prism-bash'),
+                  import('prismjs/components/prism-c'),
+                  import('prismjs/components/prism-cpp'),
+                  import('prismjs/components/prism-csharp'),
+                  import('prismjs/components/prism-docker'),
+                  import('prismjs/components/prism-java'),
+                  import('prismjs/components/prism-js-templates'),
+                  import('prismjs/components/prism-coffeescript'),
+                  import('prismjs/components/prism-diff'),
+                  import('prismjs/components/prism-git'),
+                  import('prismjs/components/prism-go'),
+                  import('prismjs/components/prism-graphql'),
+                  import('prismjs/components/prism-handlebars'),
+                  import('prismjs/components/prism-less'),
+                  import('prismjs/components/prism-makefile'),
+                  import('prismjs/components/prism-markdown'),
+                  import('prismjs/components/prism-objectivec'),
+                  import('prismjs/components/prism-ocaml'),
+                  import('prismjs/components/prism-python'),
+                  import('prismjs/components/prism-reason'),
+                  import('prismjs/components/prism-rust'),
+                  import('prismjs/components/prism-sass'),
+                  import('prismjs/components/prism-scss'),
+                  import('prismjs/components/prism-solidity'),
+                  import('prismjs/components/prism-sql'),
+                  import('prismjs/components/prism-stylus'),
+                  import('prismjs/components/prism-swift'),
+                  import('prismjs/components/prism-wasm'),
+                  import('prismjs/components/prism-yaml')
+                ])
+                return module.Code
+              })
+            }),
+            props
+          )
+      }
+    }
   }),
   // Database block
   Collection: dynamic(() => {
