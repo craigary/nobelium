2d2e8430ade98edd0181514e7314f975bf4b8147
rizpedia <achmadrizkiramadhan0101@gmail.com>
2023-04-08 12:45:46 +0700

    hide feed

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,24 @@
-import { forwardRef, useCallback, useEffect, useRef, useState } from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { useConfig } from '@/lib/config'
-import { useLocale } from '@/lib/locale'
-import useTheme from '@/lib/theme'
+import { forwardRef, useCallback, useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useConfig } from "@/lib/config";
+import { useLocale } from "@/lib/locale";
+import useTheme from "@/lib/theme";
 
 const NavBar = () => {
-  const BLOG = useConfig()
-  const locale = useLocale()
+  const BLOG = useConfig();
+  const locale = useLocale();
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
-  ]
+    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || "/", show: true },
+    { id: 1, name: locale.NAV.ABOUT, to: "/about", show: BLOG.showAbout },
+    { id: 2, name: locale.NAV.RSS, to: "/feed", show: false },
+    { id: 3, name: locale.NAV.SEARCH, to: "/search", show: true },
+  ];
   return (
     <div className="flex-shrink-0">
       <ul className="flex flex-row">
         {links.map(
-          link =>
+          (link) =>
             link.show && (
               <li
                 key={link.id}
@@ -30,55 +30,62 @@ const NavBar = () => {
         )}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default function Header ({ navBarTitle, fullWidth }) {
-  const BLOG = useConfig()
-  const { dark } = useTheme()
+export default function Header({ navBarTitle, fullWidth }) {
+  const BLOG = useConfig();
+  const { dark } = useTheme();
 
   // Favicon
 
-  const resolveFavicon = fallback => !fallback && dark ? '/favicon.dark.png' : '/favicon.png'
-  const [favicon, _setFavicon] = useState(resolveFavicon())
-  const setFavicon = fallback => _setFavicon(resolveFavicon(fallback))
+  const resolveFavicon = (fallback) =>
+    !fallback && dark ? "/favicon.dark.png" : "/favicon.png";
+  const [favicon, _setFavicon] = useState(resolveFavicon());
+  const setFavicon = (fallback) => _setFavicon(resolveFavicon(fallback));
 
   useEffect(
     () => setFavicon(),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [dark]
-  )
+  );
 
-  const useSticky = !BLOG.autoCollapsedNavBar
-  const navRef = useRef(/** @type {HTMLDivElement} */ undefined)
-  const sentinelRef = useRef(/** @type {HTMLDivElement} */ undefined)
-  const handler = useCallback(([entry]) => {
-    if (useSticky && navRef.current) {
-      navRef.current?.classList.toggle('sticky-nav-full', !entry.isIntersecting)
-    } else {
-      navRef.current?.classList.add('remove-sticky')
-    }
-  }, [useSticky])
+  const useSticky = !BLOG.autoCollapsedNavBar;
+  const navRef = useRef(/** @type {HTMLDivElement} */ undefined);
+  const sentinelRef = useRef(/** @type {HTMLDivElement} */ undefined);
+  const handler = useCallback(
+    ([entry]) => {
+      if (useSticky && navRef.current) {
+        navRef.current?.classList.toggle(
+          "sticky-nav-full",
+          !entry.isIntersecting
+        );
+      } else {
+        navRef.current?.classList.add("remove-sticky");
+      }
+    },
+    [useSticky]
+  );
 
   useEffect(() => {
-    const sentinelEl = sentinelRef.current
-    const observer = new window.IntersectionObserver(handler)
-    observer.observe(sentinelEl)
+    const sentinelEl = sentinelRef.current;
+    const observer = new window.IntersectionObserver(handler);
+    observer.observe(sentinelEl);
 
     return () => {
-      sentinelEl && observer.unobserve(sentinelEl)
-    }
-  }, [handler, sentinelRef])
+      sentinelEl && observer.unobserve(sentinelEl);
+    };
+  }, [handler, sentinelRef]);
 
-  const titleRef = useRef(/** @type {HTMLParagraphElement} */ undefined)
+  const titleRef = useRef(/** @type {HTMLParagraphElement} */ undefined);
 
-  function handleClickHeader (/** @type {MouseEvent} */ ev) {
-    if (![navRef.current, titleRef.current].includes(ev.target)) return
+  function handleClickHeader(/** @type {MouseEvent} */ ev) {
+    if (![navRef.current, titleRef.current].includes(ev.target)) return;
 
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
-    })
+      behavior: "smooth",
+    });
   }
 
   return (
@@ -86,7 +93,7 @@ export default function Header ({ navBarTitle, fullWidth }) {
       <div className="observer-element h-4 md:h-12" ref={sentinelRef}></div>
       <div
         className={`sticky-nav group m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-12 py-8 bg-opacity-60 ${
-          !fullWidth ? 'max-w-3xl px-4' : 'px-4 md:px-24'
+          !fullWidth ? "max-w-3xl px-4" : "px-4 md:px-24"
         }`}
         id="sticky-nav"
         ref={navRef}
@@ -122,21 +129,28 @@ export default function Header ({ navBarTitle, fullWidth }) {
         <NavBar />
       </div>
     </>
-  )
+  );
 }
 
-const HeaderName = forwardRef(function HeaderName ({ siteTitle, siteDescription, postTitle, onClick }, ref) {
+const HeaderName = forwardRef(function HeaderName(
+  { siteTitle, siteDescription, postTitle, onClick },
+  ref
+) {
   return (
     <p
       ref={ref}
       className="header-name ml-2 font-medium text-gray-600 dark:text-gray-300 capture-pointer-events grid-rows-1 grid-cols-1"
       onClick={onClick}
     >
-      {postTitle && <span className="post-title row-start-1 col-start-1">{postTitle}</span>}
+      {postTitle && (
+        <span className="post-title row-start-1 col-start-1">{postTitle}</span>
+      )}
       <span className="row-start-1 col-start-1">
         <span className="site-title">{siteTitle}</span>
-        <span className="site-description font-normal">, {siteDescription}</span>
+        <span className="site-description font-normal">
+          , {siteDescription}
+        </span>
       </span>
     </p>
-  )
-})
+  );
+});
