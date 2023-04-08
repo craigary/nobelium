cd3035eb99d0dbff1baadf60d6840ae73a57232a
rizpedia <achmadrizkiramadhan0101@gmail.com>
2023-04-08 12:00:25 +0700

    fix language

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'achmadrizkiramadhan0101@gmail.com',
   link: 'https://blog.rizpedia.my.id',
   description: 'Sebuah personal blog yang memilki tujuan untuk memberi akses informasi semakin mudah',
-  lang: 'id-ID', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Jakarta', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
