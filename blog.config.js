8af29a3a76045d96326b2aef759fe2ee401c1f88
rizpedia <achmadrizkiramadhan0101@gmail.com>
2023-04-11 10:16:11 +0700

    add google site verification

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
   socialLink: 'https://twitter.com/rizkiramdhnn',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+    googleSiteVerification: 'SUjKUmzKwnPt4dgCJaGnODj0K7lGzgg0HD0xa5MJL9E' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
