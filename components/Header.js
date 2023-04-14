23485af20033679213ea7df4f3abf38bda9bb77f
SilentDepth <github@silent.land>
2023-04-14 22:41:32 +0800

    🐛 site title not centered when post title is too long

--- components/Header.js
+++ components/Header.js
@@ -129,7 +129,7 @@ const HeaderName = forwardRef(function HeaderName ({ siteTitle, siteDescription,
   return (
     <p
       ref={ref}
-      className="header-name ml-2 font-medium text-gray-600 dark:text-gray-300 capture-pointer-events grid-rows-1 grid-cols-1"
+      className="header-name ml-2 font-medium text-gray-600 dark:text-gray-300 capture-pointer-events grid-rows-1 grid-cols-1 items-center"
       onClick={onClick}
     >
       {postTitle && <span className="post-title row-start-1 col-start-1">{postTitle}</span>}
