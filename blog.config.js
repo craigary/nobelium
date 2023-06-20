c35162143887bfb85354356691f2d3869e7643a7
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-06-21 11:35:10 +1200

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'A Byte of Life',
+  author: 'Ralph Li',
+  email: 'lzh8612@outlook.com',
+  link: 'https://https://www.linkedin.com/in/ralph-li-b00916272',
+  description: 'Exploring the intersection of technology and everyday life.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
