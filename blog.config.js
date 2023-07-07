8591a260418bdb174824325fe309fe2d2c8bc0cb
gwowg <guogang73@outlook.com>
2023-07-07 10:00:23 +0800

    Update blog.config.js
    从默认配置修改个人信息

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://www.threads.net/@gwo_73',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['Blog', 'Website'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
