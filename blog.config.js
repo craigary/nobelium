0b7b93b974a756f5e59cff82b0029ede8ba8461d
xindi dai <73395671+daixd5520@users.noreply.github.com>
2023-12-13 13:58:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -38,7 +38,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: 'cusdis', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
       owner: '',
