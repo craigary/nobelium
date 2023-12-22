e5d0952e22c5fc16e1f917c463c968abaecde6ee
Ali Yorulmaz <kbr.ercenk@gmail.com>
2023-12-22 18:57:40 +0300

    Update Vercel link in the footer.

--- components/Vercel.js
+++ components/Vercel.js
@@ -1,7 +1,7 @@
 const Vercel = () => {
   return (
     <a
-      href="https://vercel.com?utm_source=Craigary&utm_campaign=oss"
+      href="https://vercel.com"
       target="_blank"
       rel="noreferrer"
       aria-label="Vercel"
