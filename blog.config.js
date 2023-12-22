fdcc1035011ae766d70b68dc8595f219ead9a17c
aliyorulmazdev <aliyorulmazdev@gmail.com>
2023-12-22 20:27:29 +0300

    test

--- blog.config.js
+++ blog.config.js
@@ -41,8 +41,8 @@ const BLOG = {
     provider: '', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: 'aydevblog-comments', // The repository of store comments
-      owner: '',
-      admin: [],
+      owner: 'aliyorulmazdev',
+      admin: ['aliyorulmazdev'],
       clientID: '3b5f2601d3760f0b9f3b',
       clientSecret: '9ac0e659cee17e202436727c5ccd574d2d90d662',
       distractionFreeMode: false
