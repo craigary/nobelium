dc77f7c557b2b93a4f09c316cca0a7c560d11677
Benjamin Millot <benjamin@epigenelabs.com>
2024-01-24 12:41:10 +0100

    Update config with correct parameters.

--- blog.config.js
+++ blog.config.js
@@ -1,11 +1,11 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'BEN\'SDEVBLOG',
+  author: 'Benjamin Millot',
+  email: 'benjamin.millot.mbi@gmail.com',
+  link: 'https://blog-three-wine.vercel.app/',
+  description: 'A software engineer\'s journey through the ever-changing tech field.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
+  timezone: 'Europe/France', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
