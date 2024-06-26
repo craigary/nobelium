d61264e66270e91b434a186e8f0fe3e56c298266
7sasuke7l <50908165+7sasuke7l@users.noreply.github.com>
2024-06-26 22:57:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: '来自A城',
+  title: 'Mathamatics',
   author: 'Edenms',
   email: '',
   link: 'blog.fromacity.com',
-  description: '分享这个世界',
+  description: 'Over the mountains,mountains.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
